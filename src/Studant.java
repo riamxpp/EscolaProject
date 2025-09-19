1bb77de2efb7b4bdac84094e96e406d0476b0f19
Riam Stefesom <riamstefeson616@gmail.com>
2025-09-19 19:45:18 -0300

    Inciando a classe Studant

---
+++ src/Studant.java
@@ -0,0 +1,33 @@
+class Studant extends Pessoa {
+    private String registration;
+    private String studant_class;
+    private String[] score;
+
+    public Studant(String name) {
+        setName(name);
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getStudant_class() {
+        return studant_class;
+    }
+
+    public void setStudant_class(String studant_class) {
+        this.studant_class = studant_class;
+    }
+
+    public String[] getScore() {
+        return score;
+    }
+
+    public void setScore(String[] score) {
+        this.score = score;
+    }
+}
