6dd6878c5dd5911bd2961cf71a4963d3b84f5d0f
Riam Stefesom <riamstefeson616@gmail.com>
2025-09-19 22:11:07 -0300

    Criando class 'School.java'

---
+++ src/School.java
@@ -0,0 +1,17 @@
+public class School {
+    private Studant[] listOfStudants;
+    private Teacher[] listOfTeaches;
+    private SchoolClass[] listOfSchoolClass;
+
+    public Studant[] getListOfStudants() {
+        return listOfStudants;
+    }
+
+    public Teacher[] getListOfTeaches() {
+        return listOfTeaches;
+    }
+
+    public SchoolClass[] getListOfSchoolClass() {
+        return listOfSchoolClass;
+    }
+}
