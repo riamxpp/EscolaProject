319a3f096ca677666f69fc1c1b44ecc5af86d55f
Riam Stefesom <riamstefeson616@gmail.com>
2025-09-20 13:01:28 -0300

    Criando método para aadicionar estudantes à uma classe, mudando o nome da variável 'listOfStudents'.

--- src/SchoolClass.java
+++ src/SchoolClass.java
@@ -3,7 +3,17 @@ import java.util.ArrayList;
 public class SchoolClass {
     private String className;
     private String responsibleTeacher;
-    private ArrayList<Student> listOfStudents = new ArrayList<Student>();
+    private Teacher[] listOfTeaches;
+    private ArrayList<Student> listOfStudentsOnClass = new ArrayList<Student>();
+    private School school;
+
+    public void addStudentOnClass(String cpf, School school) {
+        this.school = school;
+        Student student = this.school.findStudent(cpf);
+        if (student != null) {
+            this.setListOfStudentsOnClass(student);
+        }
+    }
 
     public String getClassName() {
         return className;
@@ -21,11 +31,11 @@ public class SchoolClass {
         this.responsibleTeacher = responsibleTeacher;
     }
 
-    public ArrayList<Student> getListOfStudents() {
-        return listOfStudents;
+    public ArrayList<Student> getListOfStudentsOnClass() {
+        return listOfStudentsOnClass;
     }
 
-    public void setListOfStudents(Student listOfStudents) {
-        this.listOfStudents.add(listOfStudents);
+    public void setListOfStudentsOnClass(Student student) {
+        this.listOfStudentsOnClass.add(student);
     }
 }
